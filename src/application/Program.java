8e0f6b0c9b2342c5faa3d7f79aa535c882db4531
Cleudes da Silva <cleudesprogramador80@gmail.com>
2024-01-11 02:22:41 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		LocalDate birthDate = LocalDate.parse("15/04/1988",DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(21,"Bob", "bob@gmail.com", birthDate, 3000.0, obj);
+		
+		System.out.println(seller);
 		
 		
 
